20b2e917e6ac36f6ce35798c6a908c579dbbe56a
이민석 <dat0802@gmail.com>
2022-09-25 23:31:06 +0900

    Modify: 클라이언트에서 고객 id 요청으로 API 객체 호출

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+import json
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -16,9 +18,11 @@ from .serializers import (
 def get_investment(request):
     """
     투자 화면 API
+    클라이언트에서 유저 정보를 포함하여 요청
     """
 
-    account = Account.objects.get(id=1)
+    data = json.loads(request.body)
+    account = Account.objects.get(id=data["user_id"])
     serializer = InvestmentSerializer(account)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -41,7 +45,8 @@ def get_investment_detail(request):
     투자 상세 화면 API
     """
 
-    account = Account.objects.get(id=1)
+    data = json.loads(request.body)
+    account = Account.objects.get(id=data["user_id"])
     serializer = InvestmentDetailSerializer(account)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -53,7 +58,8 @@ def get_own_stock(request):
     보유 종목 화면 API
     """
 
-    asset = Asset.objects.filter(account_id=1)
+    data = json.loads(request.body)
+    asset = Asset.objects.filter(account_id=data["account_id"])
     serializer = AssetSerializer(asset, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
