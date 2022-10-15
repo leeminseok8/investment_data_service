d11bb52691f397c6bc42ce069143c8d6409fc420
이민석 <dat0802@gmail.com>
2022-10-16 01:18:46 +0900

    Modify: Account 앱 service 레이어 분리

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,126 +1,110 @@
 import json
-import hashlib
-from typing import Dict
 from urllib.request import Request
 
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from apps.accounts.models import Account, Asset
 
-from .serializers import (
-    AssetSerializer,
-    InvestmentDetailSerializer,
-    InvestmentSerializer,
-    DepositVerificateCreateSerializer,
-    DepositUpdateSerializer,
+from apps.accounts.services.investment_services import (
+    create_signature_hash,
+    create_transfer_identifier,
+    get_user_investment,
+    get_user_investment_detail,
+    get_user_own_stock,
+    update_deposit_account,
 )
 
 from .repository.get_object import (
-    get_user_account,
-    get_user_all_account,
-    get_user_own_asset,
     get_requested_user,
-    get_requested_account,
-    get_transfer_identifier,
     get_deposit,
-    get_deposited_account,
 )
 
 
 @api_view(("GET",))
-def get_investment(request: Request) -> Dict:
+def get_investment(request: Request) -> Account:
     """
     투자 화면 API
-    클라이언트에서 유저 정보를 포함하여 요청
     """
 
-    if request.user.is_authenticated:
-        account = get_user_account(request.user)
-        serializer = InvestmentSerializer(account)
+    if request.user.is_anonymous:
+        return Response({"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response({"message": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+    account = get_user_investment(request.user)
+
+    return Response(account, status=status.HTTP_200_OK)
 
 
 @api_view(("GET",))
-def get_investment_detail(request: Request) -> Dict:
+def get_investment_detail(request: Request) -> Account:
     """
     투자 상세 화면 API
     """
 
-    if request.user.is_authenticated:
-        account = get_user_account(request.user)
-        serializer = InvestmentDetailSerializer(account)
+    if request.user.is_anonymous:
+        return Response({"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response({"message": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+    account = get_user_investment_detail(request.user)
+
+    return Response(account, status=status.HTTP_200_OK)
 
 
 @api_view(("GET",))
-def get_own_stock(request: Request) -> Dict:
+def get_own_stock(request: Request) -> Asset:
     """
     보유 종목 화면 API
     """
 
-    if request.user.is_authenticated:
-        account = get_user_account(request.user)
-        asset = get_user_own_asset(account.id)
-        serializer = AssetSerializer(asset, many=True)
+    if request.user.is_anonymous:
+        return Response({"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response({"message": "권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+    asset = get_user_own_stock(request.user)
+
+    return Response(asset, status=status.HTTP_200_OK)
 
 
 @api_view(("POST",))
 def verificate_account(request: Request) -> int:
     """
     계좌 입금 Phase1
+    Request:
+        유저 이름, 계좌번호, 입금 금액
+
+    Return:
+        요청한 데이터 유효성 검사 후 일치하면 거래정보 식별자 key 응답
     """
 
     data = json.loads(request.body)
 
     user_name = data["user_name"]
     account_number = data["account_number"]
+    amount = request.data["amount"]
 
-    if request.user.is_authenticated:
-        user = get_requested_user(user_name)
+    if request.user.is_anonymous:
+        return Response({"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        if not request.user.user_name == user.user_name:
-            return Response(
-                {"message": "본인 인증에 실패하였습니다."}, status=status.HTTP_401_UNAUTHORIZED
-            )
-
-        accounts = get_user_all_account(user)
-        request_account = get_requested_account(account_number)
-
-        for account in accounts:
-            if account.account_number == request_account.account_number:
-                break
-
-        deposit_create_data = {
-            "amount": request.data["amount"],
-            "user": user.id,
-            "account": request_account.id,
-        }
-
-        serializer = DepositVerificateCreateSerializer(data=deposit_create_data)
-
-        if serializer.is_valid():
-            serializer.save()
-            transfer_identifier = get_transfer_identifier()
-
-            return Response({"transfer_identifier": transfer_identifier})
-
+    user = get_requested_user(user_name)
+    if not request.user.user_name == user.user_name:
         return Response(
-            {"message": "인증에 실패하였습니다."}, status=status.HTTP_401_UNAUTHORIZED
+            {"message": "본인 인증에 실패하였습니다."}, status=status.HTTP_401_UNAUTHORIZED
         )
-    return Response({"message": "로그인 또는 권한이 필요합니다."})
+
+    transfer_identifier = create_transfer_identifier(user, account_number, amount)
+
+    return Response({"transfer_identifier": transfer_identifier})
 
 
 @api_view(("POST",))
 def deposit_account(request: Request) -> str:
     """
     계좌 입금 Phase2
+    Request:
+        signature : Phase1에서 검증된 데이터를 hashing한 str 값
+        transfer_identifier : 거래정보 식별자 key 값
+
+    Return:
+        성공 : True
+        실패 : False
     """
 
     data = json.loads(request.body)
@@ -128,31 +112,16 @@ def deposit_account(request: Request) -> str:
     signature = data["signature"]
     transfer_identifier = data["transfer_identifier"]
 
-    if request.user.is_authenticated:
-        transfer_user = get_deposit(transfer_identifier)
+    if request.user.is_anonymous:
+        return Response({"message": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
-        salt = "chicken"
-        signature_str = f"{transfer_user.account.account_number}{transfer_user.user.user_name}{transfer_user.amount}{salt}"
-        signature_hash = hashlib.sha3_512(signature_str.encode("utf-8")).hexdigest()
+    transfer_user = get_deposit(transfer_identifier)
+    signature_hash = create_signature_hash(transfer_user)
+    if not signature_hash == signature:
+        return Response({"message": "잘못된 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-        if not signature_hash == signature:
-            return Response({"message": "권한이 필요합니다."}, status=status.HTTP_403_FORBIDDEN)
-
-        account = get_deposited_account(transfer_user)
-
-        account_update_data = {
-            "account_name": account.account_name,
-            "account_number": account.account_number,
-            "investment_principal": account.investment_principal + transfer_user.amount,
-            "user": account.user.id,
-            "brokerage": account.brokerage.id,
-        }
-
-        serializer = DepositUpdateSerializer(instance=account, data=account_update_data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response({"status": "True"}, status=status.HTTP_201_CREATED)
+    try:
+        update_deposit_account(transfer_user)
+        return Response({"status": "True"}, status=status.HTTP_204_NO_CONTENT)
+    except:
         return Response({"status": "False"}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"message": "로그인 또는 권한이 필요합니다."})
